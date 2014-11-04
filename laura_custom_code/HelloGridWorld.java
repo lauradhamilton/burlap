2e3a68a0c5cf9318d8f0f2f6e547cb2b53260bbe
Laura <lauradhamilton@gmail.com>
2014-11-04 12:34:59 -0600

    Adding in a file that runs GridWorld but with stochastic state transitions

---
+++ laura_custom_code/HelloGridWorld.java
@@ -0,0 +1,34 @@
+import burlap.domain.singleagent.gridworld.*;
+import burlap.oomdp.core.*;
+import burlap.oomdp.singleagent.explorer.VisualExplorer;
+import burlap.oomdp.visualizer.Visualizer;
+
+public class HelloGridWorld{
+
+    public static void main(String [] args){
+
+        GridWorldDomain gw = new GridWorldDomain(10,10); //10x10 grid world
+        gw.setMapToFourRooms(); //four rooms layout
+        gw.setProbSucceedTransitionDynamics(0.8); //Stochastic transitions have an 0.8 success rate
+        Domain domain = gw.generateDomain(); //generate the grid world domain
+
+        //set up the initial state
+        State s = GridWorldDomain.getOneAgentOneLocationState(domain);
+        GridWorldDomain.setAgent(s, 0, 0);
+        GridWorldDomain.setLocation(s, 0, 10, 10);
+
+        //Create visualizer and explorer
+        Visualizer v = GridWorldVisualizer.getVisualizer(gw.getMap());
+        VisualExplorer exp = new VisualExplorer(domain, v, s);
+
+        //set control keys to use w-s-a-d
+        exp.addKeyAction("w", GridWorldDomain.ACTIONNORTH);
+        exp.addKeyAction("s", GridWorldDomain.ACTIONSOUTH);
+        exp.addKeyAction("a", GridWorldDomain.ACTIONWEST);
+        exp.addKeyAction("d", GridWorldDomain.ACTIONEAST);
+
+        exp.initGUI();
+
+    }
+
+}
